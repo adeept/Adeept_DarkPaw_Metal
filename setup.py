d7f6946bdfe08c270ab0c933b2dc7eba57b1eec9
Adeept <support@adeept.com>
2025-09-29 15:49:42 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@
 
 import os
 import time
+import subprocess
 
 username = os.popen("echo ${SUDO_USER:-$(who -m | awk '{ print $1 }')}").readline().strip() # pi
 user_home = os.popen('getent passwd %s | cut -d: -f 6'%username).readline().strip()         # home
@@ -65,19 +66,21 @@ def check_os_bit():
     _ , os_bit = run_command("getconf LONG_BIT")
     return int(os_bit)
 
+def check_systemctl_service(service_name):
+    return subprocess.run(
+            ["systemctl", "is-active", service_name],
+            capture_output=True, text=True
+        ).stdout.strip() == "active"
 
 commands_apt = [
 "sudo apt-get update",
 "sudo apt-get install python3-gpiozero python3-pigpio",
-"sudo apt-get install -y python3-opencv",
 "sudo apt-get install -y python3-pyqt5 python3-opengl",
 "sudo apt-get install -y python3-picamera2",
-"sudo apt-get install -y python3-picamera2 --no-install-recommends",
 "sudo apt-get install -y python3-opencv",
 "sudo apt-get install -y opencv-data",
 "sudo apt-get install -y python3-pyaudio",
 "sudo apt-get install -y python3-smbus",
-"sudo apt-get install -y python-dev-is-python3 python3-pip libfreetype6-dev libjpeg-dev build-essential",
 ]
 mark_apt = 0
 for x in range(3):
@@ -92,13 +95,10 @@ commands_pip_1 = [
 "sudo pip3 install adafruit-pca9685",
 "sudo pip3 install flask",
 "sudo pip3 install flask_cors",
-"sudo pip3 install imutils zmq pybase64 psutil",
 "sudo pip3 install websockets",
 "sudo pip3 install adafruit-circuitpython-busdevice adafruit-circuitpython-ads7830",
-"sudo pip3 install adafruit-circuitpython-busdevice adafruit-circuitpython-ssd1306",
 "sudo pip3 install pillow",
 "sudo pip3 install mpu6050-raspberrypi",
-"sudo -H pip3 install --upgrade luma.oled",
 "sudo pip3 install numpy",
 "sudo pip3 install pyzmq",
 "sudo pip3 install imutils zmq pybase64 psutil",
@@ -107,13 +107,10 @@ commands_pip_2 = [
 "sudo pip3 install adafruit-pca9685 --break-system-packages",
 "sudo pip3 install flask --break-system-packages",
 "sudo pip3 install flask_cors --break-system-packages",
-"sudo pip3 install imutils zmq pybase64 psutil --break-system-packages",
 "sudo pip3 install websockets==13.0 --break-system-packages",
-"sudo pip3 install adafruit-circuitpython-busdevice adafruit-circuitpython-ssd1306 --break-system-packages",
+"sudo pip3 install adafruit-circuitpython-busdevice adafruit-circuitpython-ads7830 --break-system-packages",
 "sudo pip3 install pillow --break-system-packages",
-"sudo pip3 install adafruit-circuitpython-ads7830 --break-system-packages",
 "sudo pip3 install mpu6050-raspberrypi --break-system-packages",
-"sudo -H pip3 install --upgrade luma.oled --break-system-packages",
 "sudo pip3 install numpy --break-system-packages",
 "sudo pip3 install pyzmq --break-system-packages",
 "sudo pip3 install imutils zmq pybase64 psutil --break-system-packages",
@@ -137,42 +134,102 @@ else:
         if mark_pip == 0:
             break
 
-commands_3 = [
-    "cd ~",
-    "sudo git clone https://github.com/oblique/create_ap",
-    "cd create_ap && sudo make install",
-    "sudo apt-get install -y util-linux procps hostapd iproute2 iw haveged dnsmasq"
-]
 
-mark_3 = 0
-for x in range(3):
-    for command in commands_3:
-        if os.system(command) != 0:
-            print("Error running installation step 3")
-            mark_3 = 1
-    if mark_3 == 0:
-        break
+wifi_service_name="wifi-hotspot-manager.service"
+if not check_systemctl_service(wifi_service_name):
+    # wifi and hotspot switch script
+    os.system(f"sudo cp {thisPath}/wifi_hotspot_manager.sh /home/pi")
+    os.system("sudo chmod +x /home/pi/wifi_hotspot_manager.sh")
 
 
+    wifi_service_content="""[Unit]
+Description=WiFi and Hotspot Manager Service
+After=network.target NetworkManager.service
+Wants=NetworkManager.service
+
+[Service]
+Type=oneshot
+ExecStart=/home/pi/wifi_hotspot_manager.sh  
+User=root
+RemainAfterExit=yes
+
+[Install]
+WantedBy=multi-user.target
+"""
+    # system-level services must be placed in this directory
+    wifi_service_file_path = "/etc/systemd/system/" + wifi_service_name 
+
+    try:
+        # Write to the service file (requires root privileges)
+        with open(wifi_service_file_path, "w") as f:
+            f.write(wifi_service_content)
+        print(f"Service file created: {wifi_service_file_path}")
+
+        # Set file permissions
+        os.chmod(wifi_service_file_path, 0o644)
+
+        # Reload systemd configuration, enable and start the service
+        subprocess.run(["sudo", "systemctl", "daemon-reload"], check=True)
+        subprocess.run(["sudo", "systemctl", "enable", wifi_service_name], check=True)
+
+        print(f"Service {wifi_service_name} has been enabled and started")
+    except subprocess.CalledProcessError as e:
+        print(f"Command execution failed: {e}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 
-try:
-    replace_num("/boot/config.txt", '#dtparam=i2c_arm=on','dtparam=i2c_arm=on\nstart_x=1\n')
-except:
-    print('Error updating boot config to enable i2c. Please try again.')
+robot_service_name="Adeept_Robot.service"
+if not check_systemctl_service(robot_service_name):
+    # auto start script
+    try:
+        os.system("sudo touch /"+ user_home +"/startup.sh")
+        with open("/"+ user_home +"/startup.sh",'w') as file_to_write:
+            #you can choose how to control the robot
+            file_to_write.write("#!/bin/sh\nsleep 5\nsudo python3 " + thisPath + "/Server/WebServer.py")
+    except:
+        pass
+    os.system("sudo chmod 777 /"+ user_home +"/startup.sh")
 
+    #config systemctl service
+    # Define the content of the systemd service file
+    robot_service_content=f"""[Unit]
+Description=Auto-start robot control script
+After={wifi_service_name} 
 
+[Service]
+Type=simple
+User=root
+WorkingDirectory=/home/pi
+ExecStart=/home/pi/startup.sh  
+Restart=no
 
-try:
-    os.system("sudo touch /"+ user_home +"/startup.sh")
-    with open("/"+ user_home +"/startup.sh",'w') as file_to_write:
-        file_to_write.write("#!/bin/sh\nsleep 5\nsudo python3 " + thisPath + "/Server/WebServer.py")
-except:
-    pass
+[Install]
+WantedBy=multi-user.target
+"""
 
+    # Path for the service file (system-level services must be placed in this directory)
+    robot_service_file_path = "/etc/systemd/system/" + robot_service_name 
 
-os.system("sudo chmod 777 /"+ user_home +"/startup.sh")
-replace_num('/etc/rc.local','fi','fi\n/'+ user_home +'/startup.sh start')
+    try:
+        # Write to the service file (requires root privileges)
+        with open(robot_service_file_path, "w") as f:
+            f.write(robot_service_content)
+        print(f"Service file created: {robot_service_file_path}")
+
+        # Set file permissions
+        os.chmod(robot_service_file_path, 0o644)
+
+        # Reload systemd configuration, enable and start the service
+        subprocess.run(["sudo", "systemctl", "daemon-reload"], check=True)
+        subprocess.run(["sudo", "systemctl", "enable", robot_service_name], check=True)
+
+        print(f"Service {robot_service_name} has been enabled and started")
+    except subprocess.CalledProcessError as e:
+        print(f"Command execution failed: {e}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
 
 print('The program in Raspberry Pi has been installed, disconnected and restarted. \nYou can now power off the Raspberry Pi to install the camera and driver board (Robot HAT). \nAfter turning on again, the Raspberry Pi will automatically run the program to set the servos port signal to turn the servos to the middle position, which is convenient for mechanical assembly.')
 print('restarting...')
